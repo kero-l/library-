aa534ea33a0f14ee71053a02bb46ef6a01b98633
kero-l <95300842+kero-l@users.noreply.github.com>
2021-11-30 19:08:05 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("County Library\n");
+    printf("Welcome Mr. Kelvin");
+    printf("What would like to do\n");
+    printf("1. View user\n");
+    printf("2. Add new user\n");
+    printf("3. Edit user\n");
+    printf("4. Delete user\n");
+    printf("5. Change Password\n");
+    printf("6. Log out\n");
+    printf("7. Exit System\n");
+
+    return 0;
+}
